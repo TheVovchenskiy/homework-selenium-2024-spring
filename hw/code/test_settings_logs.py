3ef8f0ed5c8d28e7194206920d5473b0b9eb79cd
Vladimir Konoplyuk <v.konoplyuk@vk.team>
2024-05-14 19:04:28 +0300

    add tests for reset filters

---
+++ hw/code/test_settings_logs.py
@@ -0,0 +1,89 @@
+import time
+from typing import Optional
+from _pytest.fixtures import FixtureRequest
+from selenium.common.exceptions import TimeoutException
+
+from base import LoginCase
+from test_settings import SettingsCase
+from ui.locators.base_locators import Locator
+from ui.locators.settings_locators import SettingsPageLocators
+from ui.locators.settings_logs import SettingsLogsPageLocators as locators
+from ui.pages.settings_page import SettingsPage
+from ui.pages import settings_page
+
+from ui.fixtures import *
+
+
+ERR_ACCOUNT_NOT_FOUND = 'Не нашли такой аккаунт. Проверьте, правильно ли введён ID'
+ERR_ACCOUNT_DOES_NOT_EXIST = 'Кабинета с таким ID не существует'
+
+
+class SettingsLogsCase(SettingsCase):
+    def change_section(self):
+        self.settings_page.click(locator=locators.TAB_ITEM)
+
+        self.settings_page.wait_until_loaded([
+            locators.FILTER_BUTTON,
+        ])
+
+    def checkboxes_checked_count(self):
+        return sum(map(
+            lambda elem: elem.is_selected(),
+            self.settings_page.find_all(locators.CHECKBOX),
+        ))
+
+
+class TestSettingsLogs(SettingsLogsCase):
+    @pytest.fixture
+    def filters_modal(self):
+        try:
+            if self.settings_page\
+                .find(locators.RESET_ALL_BUTTON, timeout=1)\
+                    .is_displayed():
+                self.settings_page.click(locators.RESET_ALL_BUTTON)
+        except TimeoutException:
+            pass
+
+        self.settings_page.click(locator=locators.FILTER_BUTTON)
+
+        time.sleep(0.5)
+        assert self.settings_page\
+            .find(locators.FILTER_MODAL)\
+            .is_displayed()
+
+    @pytest.mark.skip('skip')
+    def test_checkboxes_reset(self, filters_modal):
+        checkbox_buttons = self.settings_page\
+            .find_all(locators.CHECKBOX_BUTTON)
+
+        self.settings_page.click(elem=checkbox_buttons[0])
+
+        assert self.settings_page\
+            .find(locators.RESET_BUTTON).is_displayed()
+
+        self.settings_page.click(locator=locators.WHAT_CHANGED_BUTTON)
+
+        time.sleep(1)
+        checkbox_buttons = self.settings_page\
+            .find_all(locators.CHECKBOX_BUTTON)
+
+        # assert len(checkbox_buttons) == 39
+
+        self.settings_page.click(elem=checkbox_buttons[0])
+
+        assert self.settings_page\
+            .find(locators.RESET_BUTTON).is_displayed()
+        assert self.settings_page\
+            .find(locators.RESET_ALL_BUTTON).is_displayed()
+
+        self.settings_page.click(locator=locators.RESET_BUTTON)
+        assert self.checkboxes_checked_count() == 0
+
+        self.settings_page.click(locator=locators.OBJECT_TYPE_BUTTON)
+        assert self.checkboxes_checked_count() == 1
+
+        self.settings_page.click(locator=locators.WHAT_CHANGED_BUTTON)
+
+        self.settings_page.click(locator=locators.RESET_ALL_BUTTON)
+        self.settings_page.click(locator=locators.OBJECT_TYPE_BUTTON)
+        assert self.checkboxes_checked_count() == 0
---
+++ hw/code/ui/locators/settings_logs.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.common.by import By
+
+
+class SettingsLogsPageLocators:
+    TAB_ITEM = (By.ID, 'tab-settings.logs')
+
+    FILTER_BUTTON = (By.XPATH, '//*[@data-testid="filter-button"]')
+
+    @staticmethod
+    def filter_section_button(text: str):
+        return (By.XPATH, f'//button[span[span[span[contains(text(), "{text}")]]]]')
+
+    FILTER_MODAL = (By.XPATH, '//div[@data-popper-reference-hidden and @data-popper-escaped and @data-popper-placement and @data-popper-interactive]')
+
+    OBJECT_TYPE_BUTTON = filter_section_button('Тип объекта')
+    WHAT_CHANGED_BUTTON = filter_section_button('Что изменилось')
+    CHANGE_AUTHOR_BUTTON = filter_section_button('Автор изменения')
+
+    CHECKBOX = (By.XPATH, '//input[@type="checkbox"]')
+    CHECKBOX_BUTTON = (By.XPATH, CHECKBOX[1] + '/../..')
+
+    @staticmethod
+    def modal_button(text: str):
+        return (By.XPATH, f'//button[span[span[contains(text(), "{text}")]]]')
+
+    SELECT_ALL_BUTTON = modal_button('Выбрать все')
+    RESET_BUTTON = modal_button('Сбросить')
+    RESET_ALL_BUTTON = modal_button('Сбросить все')
+
+    SAVE_BUTTON = modal_button('Применить')
+    CANCEL_BUTTON = modal_button('Отмена')
+    
