6ad22f5f6e981efa0d5d5275d8851cff31d1491e
Vladimir Konoplyuk <v.konoplyuk@vk.team>
2024-05-14 17:21:33 +0300

    add tests for checkboxes

--- hw/code/test_settings_access.py
+++ hw/code/test_settings_access.py
@@ -14,6 +14,10 @@ from ui.pages import settings_page
 from ui.fixtures import *
 
 
+ERR_ACCOUNT_NOT_FOUND = 'Не нашли такой аккаунт. Проверьте, правильно ли введён ID'
+ERR_ACCOUNT_DOES_NOT_EXIST = 'Кабинета с таким ID не существует'
+
+
 class SettingsAccessCase(SettingsCase):
     def change_section(self):
         self.settings_page.click(locator=locators.TAB_ITEM)
@@ -33,3 +37,56 @@ class TestSettingsAccess(SettingsAccessCase):
         print(self.settings_page.driver.current_window_handle)
         print(self.settings_page.driver.current_url)
 
+    @pytest.fixture
+    def modal(self):
+        self.settings_page.click(locator=locators.ADD_CABINET_BUTTON)
+
+        time.sleep(0.5)
+        assert self.settings_page\
+            .find(locators.ADD_CABINET_MODAL)\
+            .is_displayed()
+
+    @pytest.mark.skip('skip')
+    def test_add_cabinet_id(self, modal):
+        for id, expected_value, expected_error in [
+            ('4528764518', '4528764518', ERR_ACCOUNT_DOES_NOT_EXIST),
+            ('7', '7', ERR_ACCOUNT_NOT_FOUND),
+            ('12345678901', '1234567890', ERR_ACCOUNT_NOT_FOUND),
+            ('123abc', '123', ERR_ACCOUNT_NOT_FOUND),
+        ]:
+            _, curr_value = self.settings_page.update_input_field(
+                id,
+                locator=locators.ACCOUNT_ID_INPUT,
+            )
+
+            assert curr_value == expected_value
+
+            self.settings_page.click(locator=locators.SAVE_BUTTON)
+
+            assert self.error_match(
+                locators.ACCOUNT_ID_BLOCK,
+                expected_error,
+            )
+
+    @pytest.mark.skip('skip')
+    def test_add_cabinet_checkboxes(self, modal):
+        assert len(self.settings_page.find_all(locators.CHECKBOX)) == 2
+
+        self.settings_page.click(
+            locator=locators.FULL_ACCESS_RADIOBUTTON,
+        )
+        time.sleep(0.5)
+
+        assert len(self.settings_page.find_all(locators.CHECKBOX)) == 2
+
+        for button, checkbox in zip(
+            self.settings_page.find_all(locators.CHECKBOX_BUTTON),
+            self.settings_page.find_all(locators.CHECKBOX),
+        ):
+            if checkbox.is_selected():
+                self.settings_page.click(elem=button)
+
+        assert sum(map(
+            lambda elem: elem.is_selected(),
+            self.settings_page.find_all(locators.CHECKBOX),
+        )) == 1
--- hw/code/ui/locators/settings_access.py
+++ hw/code/ui/locators/settings_access.py
@@ -7,3 +7,19 @@ class SettingsAccessPageLocators:
     MORE_BUTTON = (By.XPATH, '//*[@href="/help/articles/additionalaccounts"]')
 
     ADD_CABINET_BUTTON = (By.XPATH, '//*[@data-testid="add-user"]')
+
+    ADD_CABINET_MODAL = (By.XPATH, '//div[@aria-modal="true"]')
+
+    ACCOUNT_ID_INPUT = (By.XPATH, '//*[@data-testid="userid-input"]')
+    ACCOUNT_ID_BLOCK = (By.XPATH, ACCOUNT_ID_INPUT[1] + '/../..')
+
+    READONLY_ACCESS_INPUT = (By.XPATH, '//input[@name="access" and @value="vkads_full_readonly_access"]')
+    READONLY_ACCESS_RADIOBUTTON = (By.XPATH, READONLY_ACCESS_INPUT[1] + '/..')
+
+    FULL_ACCESS_INPUT = (By.XPATH, '//input[@name="access" and @value="full_access"]')
+    FULL_ACCESS_RADIOBUTTON = (By.XPATH, FULL_ACCESS_INPUT[1] + '/..')
+
+    CHECKBOX = (By.XPATH, '//input[@type="checkbox"]')
+    CHECKBOX_BUTTON = (By.XPATH, CHECKBOX[1] + '/..')
+
+    SAVE_BUTTON = (By.XPATH, '//*[@data-testid="submit"]')
