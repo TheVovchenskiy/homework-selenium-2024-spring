cf2a1d56e1a2498d83bac61baa27dff4fb8acb0a
Vladimir Konoplyuk <v.konoplyuk@vk.team>
2024-05-26 00:53:27 +0300

    добавить локаторы лид-форм

---
+++ hw/code/ui/locators/lead_form_locators.py
@@ -0,0 +1,144 @@
+from selenium.webdriver.common.by import By
+
+from ui.locators.base_locators import Locator, locator_xpath_parent
+
+
+class LidFormLocators:
+    CREATE_BUTTON = (By.XPATH, '//*[@test-id="create-leadform-button"]')
+
+    LEAD_FORM_LIST_ITEM = (By.XPATH, '//div[@data-entityid]')
+
+    def LEAD_FORM_ITEM_NAME(name: str) -> Locator:
+        return (By.XPATH, f'//button[text()="{name}"]')
+    
+    def LEAD_FORM_ITEM_DELETE_BUTTON(name: str) -> Locator:
+        return (By.XPATH, f'//button[text()="{name}"]/../div/div/button[span/text()="Удалить"]')
+    
+    def LEAD_FORM_ITEM_Edit_BUTTON(name: str) -> Locator:
+        return (By.XPATH, f'//button[text()="{name}"]/../div/div/button[span/text()="Редактировать"]')
+    
+    DELETE_LEAD_FORM_BUTTON = (By.XPATH, '//button[span/span/text()="Удалить"]')
+    CANCEL_DELETE_LEAD_FORM_BUTTON = (By.XPATH, '//button[span/span/text()="Отменить"]')
+
+    MODAL = (By.CLASS_NAME, 'ModalRoot_componentWrapper__uzHTL')
+
+    SUBMIT_BUTTON = (By.XPATH, '//*[@data-testid="submit"]')
+    SUBMIT_BUTTON = (By.XPATH, '//*[@data-testid="cancel"]')
+
+    # First step
+    LID_FORM_NAME_INPUT = (By.XPATH, '//input[@placeholder="Название лид-формы"]')
+    
+    ADD_LOGO_BUTTON = (By.XPATH, '//*[@data-testid="set-global-image"]')
+    FILE_INPUT = (By.XPATH, '//input[@type="file"]')
+    LOGO_PREVIEW = (By.CLASS_NAME, 'Preview_activeLogoWrap__Ti7Cw')
+    CHANGE_LOGO_BUTTON = (By.XPATH, '//*[@data-testid="change-image"]')
+    CROP_LOGO_BUTTON = (By.XPATH, '//*[@data-testid="crop-image"]')
+
+    COMPANY_NAME_INPUT = (By.XPATH, '//input[@placeholder="Название компании"]')
+
+    # Compact
+    COMPACT_LABEL = (By.XPATH, '//input[@value="compact"]/..')
+    HEADER_TEXT_INPUT = (By.XPATH, '//input[@placeholder="Текст заголовка"]')
+    SHORT_DESCRIPTION_INPUT = (By.XPATH, '//input[@placeholder="Краткое описание опроса"]')
+
+    # Long text
+    LONG_TEXT_LABEL = (By.XPATH, '//input[@value="long_text"]/..')
+    LONG_DESCRIPTION_INPUT = (By.XPATH, '//textarea[@placeholder="Расскажите о вашем опросе или предложении"]')
+
+    # Award
+    AWARD_LABEL = (By.XPATH, '//input[@value="award"]/..')
+
+    DISCOUNT_INPUT = (By.XPATH, '//input[@value="discount"]')
+    DISCOUNT_VALUE_INPUT = (By.XPATH, '//input[@value="500"]')  # 500 is default
+    MONEY_DISCOUNT_BUTTON = (By.XPATH, '//input[@value="money"]/..')
+    PERCENT_DISCOUNT_BUTTON = (By.XPATH, '//input[@value="percent"]/..')
+
+    BONUS_INPUT = (By.XPATH, '//input[@value="bonus"]')
+    BONUS_VALUE_INPUT = (By.XPATH, '//input[@placeholder="Бонус"]')  # 500 is default
+
+    ACTIVE_COLOR = (By.CLASS_NAME, 'GradientSelector_roundActive__JmsvQ')
+    STYLE_RED = (By.XPATH, '//*[@data-id="0"]')
+
+    SET_MAIN_IMAGE_BUTTON = (By.XPATH, '//*[@data-testid="set-main-image"]')
+
+    # Second step
+    ADD_QUESTION_BUTTON = (By.XPATH, '//span[contains(text(), "Добавить вопрос")]/../..')
+
+    # QUESTIONS_CONTAINER = (By.XPATH, '//div[@data-rbd-droppable-id="droppable"]')
+    def QUESTIONS_CONTAINER(question_num: int) -> Locator:
+        return (By.XPATH, f'//div[contains(text(), "Вопрос № {question_num}")]/../../..')
+    
+    def REMOVE_QUESTION_BUTTON(question_num: int) -> Locator:
+        return (By.XPATH, f'//div[div/text()="Вопрос № {question_num}"]/div/button[@aria-label="Remove"]')
+
+    QUESTION_TEXT = (By.XPATH, '//textarea[@placeholder="Напишите вопрос"]')
+    QUESTION_TYPE_BUTTON = (By.XPATH, '//div[contains(text(), "Выбор одного ответа")]')
+
+    QUESTION_TYPE_FLOATING_TOOLTIP = (By.XPATH, '//div[contains(@id, "floating-ui-") and @role="tooltip"]')
+    ONE_ANSWER_BUTTON = (By.XPATH, '//span[contains(text(), "Выбор одного ответа")]/../../..')
+    MULTIPLE_ANSWER_BUTTON = (By.XPATH, '//span[contains(text(), "Выбор нескольких ответов")]/../../..')
+    FREE_FORM_ANSWER_BUTTON = (By.XPATH, '//span[contains(text(), "Ответ в произвольной форме")]/../../..')
+
+    ANSWER_INPUT = (By.XPATH, '//input[@placeholder="Введите ответ"]')
+    ANSWER_REMOVE = (By.XPATH, '//div[@class="Answer_answer__t-JwF"]/button[@aria-label="Remove"]')
+
+    ADD_ANSWER_BUTTON = (By.XPATH, '//button[span/span/text()="Добавить ответ"]')
+    ANSWER_HINT_BUTTON = (By.XPATH, '//div[@class="Hint_hintTrigger__ixYRu"]/button')
+    ANSWER_HINT_TOOLTIP = (By.XPATH, '//div[contains(@class, "Tooltip_tooltipContainer__P1b-O") and div[@role="list"]]')
+    ANSWER_HINT_ITEM = (By.CLASS_NAME, 'vkuiCell')
+
+    CONTACT_INFO_CONTAINER = (By.XPATH, '//div[h4/text()="Контактная информация"]/div/div[@data-rbd-droppable-id="droppable"]')
+    ADD_CONTACT_INFO_BUTTON = (By.XPATH, '//button[contains(@class, "Questions_addContactFieldsBtn")]')
+
+    FIRST_NAME_DELETE_BUTTON = (By.XPATH, '//button[@data-id="first_name" and @aria-label="Delete"]')
+    LAST_NAME_DELETE_BUTTON = (By.XPATH, '//button[@data-id="last_name" and @aria-label="Delete"]')
+    EMAIL_DELETE_BUTTON = (By.XPATH, '//button[@data-id="email" and @aria-label="Delete"]')
+    PHONE_DELETE_BUTTON = (By.XPATH, '//button[@data-id="phone" and @aria-label="Delete"]')
+    SOCIAL_MEDIA_PROFILE_DELETE_BUTTON = (By.XPATH, '//button[@data-id="social_media_profile" and @aria-label="Delete"]')
+    AGE_DELETE_BUTTON = (By.XPATH, '//button[@data-id="age" and @aria-label="Delete"]')
+    BIRTH_DATE_DELETE_BUTTON = (By.XPATH, '//button[@data-id="birth_date" and @aria-label="Delete"]')
+    COUNTRY_DELETE_BUTTON = (By.XPATH, '//button[@data-id="country" and @aria-label="Delete"]')
+    CITY_DELETE_BUTTON = (By.XPATH, '//button[@data-id="city" and @aria-label="Delete"]')
+
+    CONTACT_INFO_MODAL = (By.XPATH, '//div[contains(@id, "_modal_") and @aria-labelledby]')
+
+    FIRST_NAME_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="first_name"]')
+    LAST_NAME_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="last_name"]')
+    EMAIL_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="email"]')
+    PHONE_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="phone"]')
+    SOCIAL_MEDIA_PROFILE_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="social_media_profile"]')
+    AGE_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="age"]')
+    BIRTH_DATE_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="birth_date"]')
+    COUNTRY_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="country"]')
+    CITY_CHECKBOX = (By.XPATH, '//input[@type="checkbox" and @value="city"]')
+
+    SUBMIT_ADD_CONTACT_INFO_BUTTON = (By.XPATH, '//button[span/span/text()="Добавить"]')
+
+    ERROR_BANNER = (By.XPATH, '//p[text()="Минимальное количество полей: 1"]')
+
+    # Third step
+    HEADER_INPUT = (By.XPATH, '//input[@value="Спасибо за ответы!"]')
+    DESCRIPTION_INPUT = (By.XPATH, '//input[@value="Заявка отправлена"]')
+
+    ADD_SITE_BUTTON = (By.XPATH, '//div[@role="button" and @data-testid="add-site-btn"]')
+    SITE_INPUT = (By.XPATH, '//div[@placeholder="Введите ссылку на сайт"]/span/input')
+
+    ADD_PHONE_BUTTON = (By.XPATH, '//div[@role="button" and @data-testid="add-phone-btn"]')
+    PHONE_INPUT = (By.XPATH, '//input[@placeholder="+7......"]')
+
+    ADD_PROMO_CODE_BUTTON = (By.XPATH, '//div[@role="button" and @data-testid="add-promo-code-btn"]')
+    PROMO_CODE_INPUT = (By.XPATH, '//input[@placeholder="Введите промокод"]')
+
+    # Fourth step
+    NOTIFY_EMAIL_CHECKBOX = (By.XPATH, '//span[text()="Уведомлять о новых заявках по email"]/../../../input[@type="checkbox"]')
+    NOTIFY_EMAIL_BUTTON = (By.XPATH, '//span[text()="Уведомлять о новых заявках по email"]/../../../input[@type="checkbox"]/..')
+
+    NOTIFY_MESSENGER_CHECKBOX = (By.XPATH, '//span[text()="Уведомлять о новых заявках в VK Messenger"]/../../../input[@type="checkbox"]')
+    EMAILS_INPUT = (By.XPATH, '//input[@placeholder="email@example.com"]')
+
+    NOTIFY_MESSENGER_BUTTON = (By.XPATH, '//span[text()="Уведомлять о новых заявках в VK Messenger"]/../../../input[@type="checkbox"]/..')
+
+    NAME_INPUT = (By.XPATH, '//input[@placeholder="Введите фамилию, имя и отчество"]')
+    ADDRESS_INPUT = (By.XPATH, '//input[@placeholder="Введите адрес"]')
+    EMAIL_INPUT = (By.XPATH, '//input[@placeholder="Введите email"]')
+    INN_INPUT = (By.XPATH, '//input[@placeholder="Введите ИНН"]')
---
+++ hw/code/ui/pages/lead_form_page.py
@@ -0,0 +1,186 @@
+from ui.pages.base_page import DEFAULT_TIMEOUT, MAX_RETRIES_COUNT
+from ui.locators.base_locators import Locator
+from ui.locators.settings_locators import SettingsPageLocators
+from ui.pages.main_page import MainPage
+from retry import retry
+
+
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+
+
+ERR_REQUIRED_FIELD = 'Обязательное поле'
+ERR_ONLY_SPACES = 'Значение не может содержать только пробелы'
+
+ERR_INVALID_PHONE_NUMBER = 'Некорректный номер телефона'
+ERR_INVALID_PHONE_LENGTH = 'Телефон не может быть короче 12 цифр'
+
+ERR_INVALID_EMAIL = 'Некорректный email адрес'
+
+ERR_INVALID_NAME_SYMBOLS = 'Некорректные символы. Разрешена только кириллица дефис и пробел'
+
+ERR_INVALID_INN_LENGTH = 'Длина ИНН должна быть 12 символов'
+ERR_INVALID_INN = 'Невалидный ИНН'
+ERR_INCORRECT_INN = 'Некорректный ИНН'
+
+ERR_API_INVALID_NAME = 'Некорректное имя'
+
+ERR_API_INVALID_PHONE = 'Некорректный телефон'
+ERR_API_INCORRECT_PHONE = 'Некорректный формат. Пример: +71234567890'
+
+ERR_API_INVALID_EMAIL = 'Некорректный email'
+ERR_API_INCORRECT_EMAIL = 'Некорректный формат. Пример: example@mail.ru'
+
+DEFAULT_PHONE = '+71234567890'
+DEFAULT_NAME = 'Иван'
+DEFAULT_INN = '123456789012'
+DEFAULT_CABINET = 'кабинет'
+
+
+class LidFormPage(MainPage):
+    url = 'https://ads.vk.com/hq/leadads/leadforms'
+
+    def save_cancel_is_visible(self, timeout: float = DEFAULT_TIMEOUT) -> bool:
+        return self.wait(timeout).until(EC.visibility_of_element_located(
+            SettingsPageLocators.SAVE_BUTTON,
+        )).is_displayed() and self.wait(timeout).until(EC.visibility_of_element_located(
+            SettingsPageLocators.CANCEL_BUTTON,
+        )).is_displayed()
+
+    def save_cancel_is_invisible(self) -> bool:
+        return not self.wait().until(EC.invisibility_of_element_located(
+            SettingsPageLocators.SAVE_BUTTON,
+        )).is_displayed() and not self.wait().until(EC.invisibility_of_element_located(
+            SettingsPageLocators.CANCEL_BUTTON,
+        )).is_displayed()
+
+    def press_button(self, locator: Locator):
+        self.click(
+            elem=self.wait().until(EC.visibility_of_element_located(locator)),
+        )
+
+    @retry(tries=MAX_RETRIES_COUNT)
+    def press_save(self, expect_save=False, timeout=DEFAULT_TIMEOUT):
+        if self.save_cancel_is_visible(timeout):
+            self.click(locator=SettingsPageLocators.SAVE_BUTTON,
+                       timeout=timeout)
+            if expect_save:
+                if not self.wait(timeout).until(EC.invisibility_of_element(SettingsPageLocators.SAVE_BUTTON)).is_displayed():
+                    return
+
+    def press_cancel(self):
+        if self.save_cancel_is_visible():
+            self.press_button(SettingsPageLocators.CANCEL_BUTTON)
+
+    def press_add_email(self):
+        self.press_button(SettingsPageLocators.ADD_EMAIL_BUTTON)
+
+    def get_error(self, locator: Locator = None, timeout: float = DEFAULT_TIMEOUT) -> WebElement:
+        return self.find(
+            SettingsPageLocators.ERROR_MESSAGE,
+            locator_to_find_in=locator,
+            timeout=timeout,
+        )
+
+    def get_phone_error(self, locator: Locator) -> WebElement:
+        return self.get_error(SettingsPageLocators.PHONE_BLOCK)
+
+    def update_phone_number(self, new_phone_number: str) -> tuple[str, str]:
+        return self.update_input_field(
+            new_phone_number,
+            locator=SettingsPageLocators.PHONE_INPUT,
+        )
+
+    def update_name(self, new_name: str) -> tuple[str, str]:
+        return self.update_input_field(
+            new_name,
+            locator=SettingsPageLocators.NAME_INPUT,
+        )
+
+    def update_inn(self, new_inn: str) -> tuple[str, str]:
+        return self.update_input_field(
+            new_inn,
+            locator=SettingsPageLocators.INN_INPUT,
+        )
+
+    def update_cabinet(self, new_cabinet: str) -> tuple[str, str]:
+        return self.update_input_field(
+            new_cabinet,
+            locator=SettingsPageLocators.CABINET_INPUT,
+        )
+
+    def update_email(self, id: int, email: str):
+        return self.update_input_field(
+            email,
+            locator=SettingsPageLocators.ADDITIONAL_EMAIL_INPUT(id),
+        )
+
+    def has_warning(self, message: str) -> bool:
+        elem = self.wait().until(EC.visibility_of(
+            self.find(SettingsPageLocators.WARNING, timeout=5)))
+
+        return message in elem.text
+
+    def remove_additional_email(self):
+        self.click(locator=SettingsPageLocators.REMOVE_EMAIL_BUTTON, timeout=5)
+
+    def open_language_dropdown(self):
+        elem = self.find(SettingsPageLocators.LANGUAGE_BUTTON)
+
+        id = elem.get_attribute('aria-owns')
+        self.click(elem=elem)
+        if self.wait().until(EC.visibility_of_element_located(
+            SettingsPageLocators.LANGUAGE_DROPDOWN(id)
+        )).is_displayed():
+            return id
+
+    def get_curr_language(self) -> str:
+        elem = self.wait().until(EC.visibility_of_element_located(
+            SettingsPageLocators.LANGUAGE_CURR_LANG))
+
+        return elem.text
+
+    def assert_chosen_language(self, lang_elem: WebElement):
+        assert lang_elem.get_attribute('aria-selected') == 'true'
+
+    def scroll_to_connected_cabinet(self):
+        self.scroll_to(SettingsPageLocators.CONNECT_CABINET)
+
+    def change_language(self):
+        self.scroll_to(SettingsPageLocators.LANGUAGE_BUTTON)
+
+        curr_language = self.get_curr_language()
+
+        id = self.open_language_dropdown()
+        print(curr_language)
+
+        if curr_language == 'RU':
+            ru_elem_ru = self.wait().until(EC.visibility_of_element_located(
+                SettingsPageLocators.LANGUAGE_RU(id)
+            ))
+            self.assert_chosen_language(ru_elem_ru)
+
+            self.click(locator=SettingsPageLocators.LANGUAGE_EN(id))
+
+            id = self.open_language_dropdown()
+
+            ru_elem_en = self.wait().until(EC.visibility_of_element_located(
+                SettingsPageLocators.LANGUAGE_EN(id)
+            ))
+            self.assert_chosen_language(ru_elem_en)
+        elif curr_language == 'EN':
+            en_elem_en = self.wait().until(EC.visibility_of_element_located(
+                SettingsPageLocators.LANGUAGE_EN(id)
+            ))
+            self.assert_chosen_language(en_elem_en)
+
+            self.click(locator=SettingsPageLocators.LANGUAGE_RU(id))
+
+            id = self.open_language_dropdown()
+
+            en_elem_ru = self.wait().until(EC.visibility_of_element_located(
+                SettingsPageLocators.LANGUAGE_RU(id)
+            ))
+            self.assert_chosen_language(en_elem_ru)
+
+        self.click(locator=SettingsPageLocators.LANGUAGE_BUTTON)
